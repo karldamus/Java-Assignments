77a96b06ddf5325783545b6c880bfa966612ac95
karldamus <getaholdofkarl@gmail.com>
2021-02-25 13:01:20 -0500

    Create Scale.java

---
+++ assignment02/Scale.java
@@ -0,0 +1,9 @@
+/*****************************
+ * Scale.java
+ * Author: Jason Hinek 
+ * ©2021, All Rights Reserved
+*****************************/
+
+public enum Scale {
+	CELSIUS, FAHRENHEIT, KELVIN, NONE;
+}
